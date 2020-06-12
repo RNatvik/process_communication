63488ca83fa480e0da89393676d4b76734aa74cc
Ruben <ronatvik@gmail.com>
2020-06-13 00:36:17 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="process-communication-pkg",
+    name="process-communication",
     version="0.0.1",
     author="Ruben Natvik",
     author_email="ronatvik@gmail.com",
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/RNatvik/process_communication",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=('examples',)),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
