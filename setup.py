5ae4f70fd686981e4f1422d1570d91b903eaf24b
Ruben <ronatvik@gmail.com>
2020-06-15 13:16:41 +0200

    Added JSONDecodeError exception handling in PublisherSocket class

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r') as fh:
 
 setuptools.setup(
     name="process-communication",
-    version="0.0.1",
+    version="0.0.2",
     author="Ruben Natvik",
     author_email="ronatvik@gmail.com",
     description="A framework for communication across separate python processes",
